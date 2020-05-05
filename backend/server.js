231b03816e14b82df50b6cdf175dd525d1c427c6
Maria Davis <mdavis81@my.bcit.ca>
2020-05-05 12:35:58 -0700

    moved build folder to backend

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,16 @@ require("./app/routes/jobPost.routes")(app);
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
+// The following code lets the server know to serve all 
+// static React files from the build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Keep our client side routing functional, essentially 
+// serves the index.html file on any unknown routes
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
