7261bd10339922ad802c4989c20906295a23376e
Maria Davis <mdavis81@my.bcit.ca>
2020-05-06 13:30:01 -0700

    db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,12 +2,12 @@ module.exports = {
     HOST: "us-cdbr-east-06.cleardb.net",
     USER: "b579dcc74221d6",
     PASSWORD: "dd453f4a",
-    DB: "heroku_72211f91d285493",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    DB: "heroku_72211f91d285493"
+  //   dialect: "mysql",
+  //   pool: {
+  //     max: 5,
+  //     min: 0,
+  //     acquire: 30000,
+  //     idle: 10000
+  //   }
   };
