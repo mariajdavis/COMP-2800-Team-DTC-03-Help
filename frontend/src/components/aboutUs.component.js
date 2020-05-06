37759de5b851685026bf96da9658ebd5bd38b8be
Ethan <esadowski1@my.bcit.ca>
2020-05-06 15:22:38 -0700

    Fixed background and footer added AboutUs page.

---
+++ frontend/src/components/aboutUs.component.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import './Layouts/AboutUs.css'
+
+export default class AboutUs extends Component {
+
+    render() {
+        return (
+        <div id="aboutUs">
+            <div id="aboutDiv">
+                <div id="headDiv">
+                    <h2 id="aboutHead">About the help! team:</h2>
+                    <p id="aboutPara">When you meet with your Client, you will be accountable for your time and show them what you have finished during the sprint. A team must always have something finished to demonstrate at the end of the sprint.</p>
+                </div>
+                <div id="itemOne">
+                    <h3>Maria Davis</h3>
+                    <p> yo</p>
+                </div>
+                <div id="itemTwo">
+                    <h3>Chase Lu</h3>
+                    <p> yo</p>
+                </div>
+                <div id="itemThree">
+                    <h3>Geun Uoo Kim</h3>
+                    <p> yo</p>
+                </div>
+                <div id="itemFour">
+                    <h3>Ethan Sadowski</h3>
+                    <p> yo</p>
+                </div>
+            </div>
+          </div>
+        );
+      }
+}
