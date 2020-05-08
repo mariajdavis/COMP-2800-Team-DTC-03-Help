05af72befd31bbc3c8727a439d1f869af74b6c71
Geun Uoo Kim <gonukimxxiv@gmail.com>
2020-05-07 19:22:18 -0700

    heroku deploy

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -4,7 +4,6 @@ export default axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:8080",
-    "Access-Control-Allow-Credentials": true
+    "Access-Control-Allow-Origin": "http://localhost:8080"
   }
 });
