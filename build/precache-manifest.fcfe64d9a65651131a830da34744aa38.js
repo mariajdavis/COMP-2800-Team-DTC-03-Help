39220116c0f4a78f21e8a9f599e1a41f99e6eb65
Maria Davis <>
2020-05-11 14:40:51 -0700

    copied build folder to root directory

---
+++ build/precache-manifest.fcfe64d9a65651131a830da34744aa38.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf9f37c760f3769020f1316a6f8ee26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dae74555fa4e772a06a",
+    "url": "/static/css/2.25d2dfdd.chunk.css"
+  },
+  {
+    "revision": "e42b09a4fdcaa36c2330",
+    "url": "/static/css/main.6ffdbe08.chunk.css"
+  },
+  {
+    "revision": "0dae74555fa4e772a06a",
+    "url": "/static/js/2.1318a042.chunk.js"
+  },
+  {
+    "revision": "6dee788c7c98203162cc160a5d6af82c",
+    "url": "/static/js/2.1318a042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e42b09a4fdcaa36c2330",
+    "url": "/static/js/main.1a19ff0e.chunk.js"
+  },
+  {
+    "revision": "07858fea14fd66493418",
+    "url": "/static/js/runtime-main.efee8004.js"
+  },
+  {
+    "revision": "fb7d453b9c617587e04813afe1fb3b30",
+    "url": "/static/media/background_frame.fb7d453b.png"
+  },
+  {
+    "revision": "20bae3d76ce5f53cae59bfb8d2700808",
+    "url": "/static/media/beach.20bae3d7.png"
+  },
+  {
+    "revision": "e086543ffaf61643d9fe51041dd86f38",
+    "url": "/static/media/catepillar.e086543f.jpg"
+  },
+  {
+    "revision": "f50267ba6214e91725596d6a93410ee8",
+    "url": "/static/media/farmer.f50267ba.JPG"
+  },
+  {
+    "revision": "f8452c9a0484c6db1f128ce235e10a0d",
+    "url": "/static/media/logo.f8452c9a.jpg"
+  },
+  {
+    "revision": "dfd26429e040b7c2c5043ad6ab4ed618",
+    "url": "/static/media/rectangle_24.dfd26429.png"
+  }
+]);
