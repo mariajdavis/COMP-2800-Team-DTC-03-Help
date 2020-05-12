9d1de4c18032da366a2c0ac77e42b7f8ed9de307
Geun Uoo Kim <gonukimxxiv@gmail.com>
2020-05-11 19:34:07 -0700

    deploy

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/*', (req, res) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = "us-cdbr-east-06.cleardb.net:3306"
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
