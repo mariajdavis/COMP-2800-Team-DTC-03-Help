d38f394d73766e51d4ef9826c75e13869a19bf27
Geun Uoo Kim <gonukimxxiv@gmail.com>
2020-05-11 21:00:23 -0700

    deploy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var corsOptions = {
   origin: "https://helpservices.herokuapp.com"
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
