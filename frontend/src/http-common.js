3b5c56c98e0543df181161f6d4ad550af75789ce
chase-lsc <chase_lsc@hotmail.com>
2020-05-18 11:59:32 -0700

    switched to local hosting for development

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://helpservices.herokuapp.com/api",
+  baseURL: "http://localhost:8080/api",
+  //baseURL: "https://helpservices.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   }
