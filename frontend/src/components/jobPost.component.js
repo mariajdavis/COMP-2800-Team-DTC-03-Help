47a54953e3a65334e1cf55a001872c5d92061cd5
Ethan <esadowski1@my.bcit.ca>
2020-05-22 13:03:42 -0700

    Edited Css

--- frontend/src/components/jobPost.component.js
+++ frontend/src/components/jobPost.component.js
@@ -32,7 +32,7 @@ export default class JobPost extends Component {
     };
   }
 
-  goBack(){
+  goBack() {
     this.props.history.goBack()
   }
 
@@ -43,7 +43,7 @@ export default class JobPost extends Component {
   onChangeTitle(e) {
     const title = e.target.value;
 
-    this.setState(function(prevState) {
+    this.setState(function (prevState) {
       return {
         currentJobPost: {
           ...prevState.currentJobPost,
@@ -55,7 +55,7 @@ export default class JobPost extends Component {
 
   onChangeDescription(e) {
     const description = e.target.value;
-    
+
     this.setState(prevState => ({
       currentJobPost: {
         ...prevState.currentJobPost,
@@ -66,7 +66,7 @@ export default class JobPost extends Component {
 
   onChangeJobType(e) {
     const jobType = e.target.value;
-    
+
     this.setState(prevState => ({
       currentJobPost: {
         ...prevState.currentJobPost,
@@ -79,7 +79,7 @@ export default class JobPost extends Component {
   onChangeStartDate(e) {
     const startDate = e.target.value;
 
-    this.setState(function(prevState) {
+    this.setState(function (prevState) {
       return {
         currentJobPost: {
           ...prevState.currentJobPost,
@@ -91,7 +91,7 @@ export default class JobPost extends Component {
 
   onChangeRate(e) {
     const rate = e.target.value;
-    
+
     this.setState(prevState => ({
       currentJobPost: {
         ...prevState.currentJobPost,
@@ -102,7 +102,7 @@ export default class JobPost extends Component {
 
   onChangeContractLength(e) {
     const contractLength = e.target.value;
-    
+
     this.setState(prevState => ({
       currentJobPost: {
         ...prevState.currentJobPost,
@@ -141,7 +141,7 @@ export default class JobPost extends Component {
       });
   }
 
-  deleteJobPost() {    
+  deleteJobPost() {
     JobPostDataService.delete(this.state.currentJobPost.id)
       .then(response => {
         console.log(response.data);
@@ -156,119 +156,121 @@ export default class JobPost extends Component {
     const { currentJobPost } = this.state;
 
     return (
-      <div id="contentLayoutAddJob">
-        <div id="contentDiv">
-        
-      <div className="card">
-        {currentJobPost ? (
+      <div id="contentLayoutRegister">
+        <div id="contentDivRegister">
           <div>
-          <h4>JobPost</h4>
-          <div>
-            <form id="addJobArea">
-              
-              <div className="form-group">
-                <label htmlFor="title"><strong>Title</strong></label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="title"
-                  value={currentJobPost.title}
-                  onChange={this.onChangeTitle}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="description"><strong>Description</strong></label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="description"
-                  value={currentJobPost.description}
-                  onChange={this.onChangeDescription}
-                />
-              </div>
+          <div id="registration-background">
+            {currentJobPost ? (
+              <div>
+                <h4>JobPost</h4>
+                <div>
+                  <form id="addJobArea">
 
-              <div className="form-group">
-                <label>
-                  <strong>Job Type: </strong>
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="jobType"
-                  value={currentJobPost.jobType}
-                  onChange={this.onChangeJobType}
-                />
-              </div>
+                    <div className="form-group">
+                      <label htmlFor="title"><strong>Title</strong></label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="title"
+                        value={currentJobPost.title}
+                        onChange={this.onChangeTitle}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="description"><strong>Description</strong></label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="description"
+                        value={currentJobPost.description}
+                        onChange={this.onChangeDescription}
+                      />
+                    </div>
 
-              <div className="form-group">
-                <label>
-                  <strong>Hourly Rate: </strong>
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="rate"
-                  value={currentJobPost.rate}
-                  onChange={this.onChangeRate}
-                />
-              </div>
+                    <div className="form-group">
+                      <label>
+                        <strong>Job Type: </strong>
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="jobType"
+                        value={currentJobPost.jobType}
+                        onChange={this.onChangeJobType}
+                      />
+                    </div>
 
-              <div className="form-group">
-                <label>
-                  <strong>Start Date: </strong>
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="startDate"
-                  value={currentJobPost.startDate}
-                  onChange={this.onChangeStartDate}
-                />
-              </div>
+                    <div className="form-group">
+                      <label>
+                        <strong>Hourly Rate: </strong>
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="rate"
+                        value={currentJobPost.rate}
+                        onChange={this.onChangeRate}
+                      />
+                    </div>
 
-              <div className="form-group">
-                <label>
-                  <strong>Contract Length: </strong>
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="contractLength"
-                  value={currentJobPost.contractLength}
-                  onChange={this.onChangeContractLength}
-                />
-              </div>
+                    <div className="form-group">
+                      <label>
+                        <strong>Start Date: </strong>
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="startDate"
+                        value={currentJobPost.startDate}
+                        onChange={this.onChangeStartDate}
+                      />
+                    </div>
 
-            
+                    <div className="form-group">
+                      <label>
+                        <strong>Contract Length: </strong>
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="contractLength"
+                        value={currentJobPost.contractLength}
+                        onChange={this.onChangeContractLength}
+                      />
+                    </div>
 
-            <button
-              className="badge badge-danger mr-2"
-              onClick={this.deleteJobPost}
-            >
-              Delete
+
+
+                    <button
+                      className="badge badge-danger mr-2"
+                      onClick={this.deleteJobPost}
+                    >
+                      Delete
             </button>
 
-            <button
-              type="submit"
-              className="badge badge-success"
-              onClick={this.updateJobPost}
-              
-            >
-              Update
+                    <button
+                      type="submit"
+                      className="badge badge-success"
+                      onClick={this.updateJobPost}
+
+                    >
+                      Update
             </button>
-            </form>
-            <p>{this.state.message}</p>
+                  </form>
+                  <p>{this.state.message}</p>
+                </div>
+              </div>
+            ) : (
+                <div>
+                  <br />
+                  <p><center>Job post deleted!</center></p>
+                </div>
+              )}
+              </div>
           </div>
-          </div>
-        ) : (
-          <div>
-            <br />
-            <p><center>Job post deleted!</center></p>
-          </div>
-        )}
-        </div>
         </div>
       </div>
+      
     );
   }
 }
